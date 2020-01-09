a2ab5d97d8e9c6805f01423add32108541491aba
Joseph McDade <jmcdade@localhost.localdomain>
2020-01-08 19:16:47 -0500

    Testing fetch

--- index.php
+++ index.php
@@ -4,5 +4,12 @@
   <?php require_once('pdoconnect.php') ?>
 </head>
 <body>
-  <h1>Hello World!!!</h1>
+  <h1>ADAs</h1>
+  <?php
+  $ada = "SELECT title,directive,ADA.primary_manager,ADA.secondary_manager FROM ADA
+    JOIN MEMBER on primary_manager=MEMBER.member_num
+    JOIN MEMBER on secondary_manager=MEMBER.member_num;";
+  $adaResult = $pdo->query($ada);
+  $adaList = $adaResult->fetch();
+  echo "Title: $adaList[0]"?>
 </body>
